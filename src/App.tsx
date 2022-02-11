a3e4101df6d63896400f9ec9f1e36797e3ad7981
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-02-11 23:57:38 +0900

    feat: useRecoilStateを使う
    Recoil練習

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, VFC} from 'react';
 import './App.css';
+import {useRecoilState} from "recoil";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {atom} from "recoil";
+
+type User = {
+    name: string
+    age: number
+}
+
+const stateUsers = atom<User[] | []>({
+    key: "state-user",
+    default: []
+})
+
+const App: VFC = () => {
+    const [name, setName] = useState('')
+    const [age, setAge] = useState(0)
+
+    const [users, setUsers] = useRecoilState(stateUsers)
+
+    const saveHandler = () => setUsers([...users, {name, age}])
+
+    return (
+        <div className="App">
+            <div>名前: <input type="text" value={name} onChange={e => setName(e.target.value)}/></div>
+            <div>年齢: <input type="text" value={age} onChange={e => setAge(Number(e.target.value))}/></div>
+            <button onClick={saveHandler}>保存</button>
+
+            <ul>
+                {users.map((user, idx) => {
+                    return <li key={idx}>{user.name} : {user.age} </li>
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {RecoilRoot} from "recoil";
+
 ReactDOM.render(
-  <React.StrictMode>
+  <RecoilRoot>
     <App />
-  </React.StrictMode>,
+  </RecoilRoot>,
   document.getElementById('root')
 );
 
