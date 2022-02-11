dda70af6e3878561995b924ae2a99db647591d5f
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-02-12 00:27:27 +0900

    refactor: radio追加

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,44 @@ import {atom} from "recoil";
 type User = {
     name: string
     age: number
+    enable: boolean
 }
 
 const stateUsers = atom<User[] | []>({
-    key: "state-user",
+    key: "stateUsers",
     default: []
 })
 
 const App: VFC = () => {
     const [name, setName] = useState('')
     const [age, setAge] = useState(0)
+    const [enable, setEnable] = useState(false)
 
     const [users, setUsers] = useRecoilState(stateUsers)
 
-    const saveHandler = () => setUsers([...users, {name, age}])
+    const saveHandler = () => setUsers([...users, {name, age, enable}])
 
     return (
         <div className="App">
             <div>名前: <input type="text" value={name} onChange={e => setName(e.target.value)}/></div>
             <div>年齢: <input type="text" value={age} onChange={e => setAge(Number(e.target.value))}/></div>
+            <div>可否:
+                <label>
+                    <input type="radio" checked={enable} onChange={e => setEnable(!!e.target.value)}/>
+                    <span>True</span>
+                </label>
+                <label>
+                    <input type="radio" checked={!enable} onChange={e => setEnable(!e.target.value)}/>
+                    <span>False</span>
+                </label>
+            </div>
+
             <button onClick={saveHandler}>保存</button>
+            <br/>
 
             <ul>
                 {users.map((user, idx) => {
-                    return <li key={idx}>{user.name} : {user.age} </li>
+                    return <li key={idx}>{user.name} : {user.age} : {user.enable.toString()} </li>
                 })}
             </ul>
         </div>
