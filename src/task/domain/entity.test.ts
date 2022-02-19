1b4c9ca33b699e81e1d4b4b78b71427e48d66b4f
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-02-19 14:32:46 +0900

    feat: selectorFamilyでEntityを表現

---
+++ src/task/domain/entity.test.ts
@@ -0,0 +1,47 @@
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil';
+import { renderHook } from '@testing-library/react-hooks';
+import { taskSelector } from './entity';
+import { useEffect } from 'react';
+import { TaskEntity } from '../types';
+
+describe('taskSelector', () => {
+  it('初期は空文字', () => {
+    const taskId = 1001;
+    const { result } = renderHook(() => useRecoilValue(taskSelector(taskId)), {
+      wrapper: RecoilRoot,
+    });
+
+    const { id, title, description, isDone } = result.current;
+    expect(id).toEqual(taskId);
+    expect(title).toEqual('');
+    expect(description).toEqual('');
+    expect(isDone).toEqual(false);
+  });
+
+  it('State反映時', () => {
+    const data: TaskEntity = {
+      id: 1001,
+      title: 'Hello World',
+      description: 'Hello World Description',
+      isDone: false,
+    };
+    const { result } = renderHook(
+      () => {
+        const [task, setTask] = useRecoilState(taskSelector(data.id));
+        useEffect(() => {
+          setTask(data);
+        }, [setTask]);
+        return task;
+      },
+      {
+        wrapper: RecoilRoot,
+      },
+    );
+
+    const { id, title, description, isDone } = result.current;
+    expect(id).toEqual(data.id);
+    expect(title).toEqual(data.title);
+    expect(description).toEqual(data.description);
+    expect(isDone).toEqual(data.isDone);
+  });
+});
---
+++ src/task/domain/entity.ts
@@ -0,0 +1,38 @@
+import { DefaultValue, selectorFamily } from 'recoil';
+import { TaskEntity, TaskId } from '../types';
+import {
+  taskDescriptionAtom,
+  taskIsDoneAtom,
+  taskTitleAtom,
+} from './valueObject';
+
+export const taskSelector = selectorFamily<TaskEntity, TaskId>({
+  key: 'TaskSelector',
+  get:
+    (taskId) =>
+    ({ get }) => {
+      return {
+        id: taskId,
+        title: get(taskTitleAtom(taskId)),
+        description: get(taskDescriptionAtom(taskId)),
+        isDone: get(taskIsDoneAtom(taskId)),
+      };
+    },
+
+  set:
+    (taskId) =>
+    ({ get, set, reset }, newValue) => {
+      if (newValue instanceof DefaultValue) {
+        // Reset呼び出し時
+        reset(taskTitleAtom(taskId));
+        reset(taskDescriptionAtom(taskId));
+        reset(taskIsDoneAtom(taskId));
+        return;
+      }
+
+      set(taskTitleAtom(taskId), newValue.title);
+      newValue.description &&
+        set(taskDescriptionAtom(taskId), newValue.description);
+      set(taskIsDoneAtom(taskId), newValue.isDone);
+    },
+});
