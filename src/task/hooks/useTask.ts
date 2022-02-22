b8bb8431ab669c4df4b50a0491d129e2cd32b300
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-02-22 10:54:40 +0900

    feat: hooksに機能追加

--- src/task/hooks/useTask.ts
+++ src/task/hooks/useTask.ts
@@ -1,13 +1,28 @@
 import { useRecoilCallback } from 'recoil';
-import { Task } from '../types';
+import { Task, TaskId } from '../types';
 import { taskSelector } from '../states/selectors';
+import { taskIdsAtom } from '../states/atoms';
 
 export const useTask = () => {
-  const setTasks = useRecoilCallback((api) => (tasks: Task[]) => {
-    tasks.forEach((task) => {
-      api.set(taskSelector(task.id), task);
-    });
-  }, []);
+  const setTasks = useRecoilCallback(
+    (api) => (tasks: Task[]) => {
+      tasks.forEach((task) => {
+        api.set(taskSelector(task.id), task);
+      });
+    },
+    [],
+  );
 
-  return {setTasks}
+  const setTask = useRecoilCallback((api) => (newTask: Task) => {
+    api.set(taskSelector(newTask.id), newTask);
+  });
+
+  const removeTask = useRecoilCallback((api) => (taskId: TaskId) => {
+    api.reset(taskSelector(taskId));
+    api.set(taskIdsAtom, (tasks) =>
+      tasks.filter((id: TaskId) => id !== taskId),
+    );
+  });
+
+  return { setTasks, setTask, removeTask };
 };
